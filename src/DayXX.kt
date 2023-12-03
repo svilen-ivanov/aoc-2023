6013a32d8fff5e1636f9f19613b3f2b0c08bcc56
Svilen Ivanov <svilen.ivanov@gmail.com>
2023-12-03 20:16:42 +0200

    Migrate from old repo

---
+++ src/DayXX.kt
@@ -0,0 +1,30 @@
+fun main() {
+    val day = "XX"
+
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day${day}_test")
+
+    val part1Expected = ""
+    val part1 = part1(testInput)
+    println("(Test) Part 1: expected: $part1Expected, got: $part1")
+
+    val part2Expected = ""
+    val part2 = part2(testInput)
+    println("(Test) Part 2: expected: $part2Expected, got: $part2")
+
+    val input = readInput("Day${day}")
+
+    val part1Real = part1(input)
+    println("(Real) Part 1: $part1Real")
+
+    val part2Real = part2(input)
+    println("(Real) Part 2: $part2Real")
+}
