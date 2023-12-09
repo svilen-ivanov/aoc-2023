d9a31df4d51b40185a231156496622a44069c37a
Svilen Ivanov <svilen.ivanov@gmail.com>
2023-12-09 07:37:10 +0200

    Day 8

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,30 +1,92 @@
 fun main() {
-    val day = "XX"
+    val day = "09"
 
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun convertReadings(readings: List<Long>): MutableList<Long> {
+        return buildList {
+            var prev: Long? = null
+            for ((i, r) in readings.withIndex()) {
+                if (i == 0) {
+                    prev = r
+                    continue
+                }
+                require(prev != null)
+                add(r - prev)
+                prev = r
+            }
+        }.toMutableList().also {
+            require(it.size == readings.size - 1)
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun processReadings(readings: List<Long>): MutableList<MutableList<Long>> {
+        val allReadings = mutableListOf<MutableList<Long>>()
+        allReadings.add(readings.toMutableList())
+        var curr = readings
+        do {
+            curr = convertReadings(curr)
+            allReadings.add(curr)
+            if (curr.all { it == 0L }) {
+                break
+            }
+        } while (true)
+        allReadings.println()
+        return allReadings
+    }
+
+    val space = Regex("\\s+")
+
+    fun predict(p: MutableList<MutableList<Long>>): Long {
+        var curr = p.last().first()
+        for (i in p.size - 2 downTo 0) {
+            val prev = p[i].first()
+            curr = prev - curr
+            p[i].add(0, curr)
+        }
+        for (x in p) {
+            x.println()
+        }
+        return curr.also { it.println() }
+    }
+
+    fun part1(input: List<String>): Long {
+        var sum = 0L
+        for (line in input) {
+            val readings = line.split(space).map { it.toLong() }
+            val p = processReadings(readings)
+            val x = predict(p)
+            sum += x.toInt()
+        }
+        return sum
+    }
+
+    fun part2(input: List<String>): Long {
+        var sum = 0L
+        for (line in input) {
+            val readings = line.split(space).map { it.toLong() }
+            val p = processReadings(readings)
+            val x = predict(p)
+            sum += x.toInt()
+        }
+
+        return sum
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day${day}_test")
 
-    val part1Expected = ""
-    val part1 = part1(testInput)
-    println("(Test) Part 1: expected: $part1Expected, got: $part1")
+//    val part1Expected = "114"
+//    val part1 = part1(testInput)
+//    println("(Test) Part 1: expected: $part1Expected, got: $part1")
 
-    val part2Expected = ""
+    val part2Expected = "2"
     val part2 = part2(testInput)
     println("(Test) Part 2: expected: $part2Expected, got: $part2")
 
     val input = readInput("Day${day}")
 
-    val part1Real = part1(input)
-    println("(Real) Part 1: $part1Real")
-
+//    val part1Real = part1(input)
+//    println("(Real) Part 1: $part1Real")
+//
     val part2Real = part2(input)
     println("(Real) Part 2: $part2Real")
 }
