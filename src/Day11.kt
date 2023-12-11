0f7cb737f9d56322c45489dbe29a2f29966c9e7a
Svilen Ivanov <svilen.ivanov@gmail.com>
2023-12-11 09:14:21 +0200

    Day 11

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,132 +1,220 @@
-data class Point(val x: Int, val y: Int)
+import kotlin.math.abs
+
+data class Point(val x: Long, val y: Long) {
+    fun distanceTo(other: Point): Long {
+        return abs(x - other.x) + abs(y - other.y)
+        +(if (x != other.x) 1 else 0) + (if (y != other.y) 1 else 0)
+    }
+}
 
 sealed class Kind {
-    abstract val coord: Point
+    abstract var coord: Point
     abstract val char: String
+    var num = 0
 
-    data class Space(override val char: String, override val coord: Point) : Kind()
-    data class Galaxy(override val char: String, override val coord: Point) : Kind()
+    data class Space(override val char: String, override var coord: Point) : Kind()
+    data class Galaxy(override val char: String, override var coord: Point) : Kind()
 }
 
 class Universe {
     var space = mutableMapOf<Point, Kind>()
+//    val grow = 9
+    val grow = (1_000_000 - 1).toLong()
     var dim: Point = Point(0, 0)
+    val galaxies by lazy { space.filterValues { it is Kind.Galaxy } as Map<Point, Kind.Galaxy> }
+    val galaxyByNumber by lazy { galaxies.values.associateBy { it.num } }
+
+    val rowsWithPoints = mutableSetOf<Long>()
+    val colsWithPoints = mutableSetOf<Long>()
+
+    val pairs by lazy {
+        val gList = galaxies.values.toList()
+        val pairs = mutableListOf<Pair<Kind.Galaxy, Kind.Galaxy>>()
+        for (g1 in 0..<gList.size) {
+            for (g2 in (g1 + 1)..<gList.size) {
+                pairs.add(Pair(gList[g1], gList[g2]))
+            }
+        }
+        pairs.toList()
+    }
+
+    fun renumber() {
+        var num = 1
+        for (g in galaxies) {
+            g.value.num = num
+            num += 1
+        }
+    }
 
     fun printSpace() {
         for (y in 0..dim.y) {
             for (x in 0..dim.x) {
                 val point = Point(x, y)
-                val kind = space.getValue(point)
-                print(kind.char)
+                val kind = space[point]
+                print(
+                    when (kind) {
+                        null, is Kind.Space -> "."
+                        is Kind.Galaxy -> kind.num
+                    }
+                )
             }
             print("\n")
         }
     }
 
-    fun shiftRow(y: Int) {
+    fun shiftRow(y: Long) {
         space = space.mapKeysTo(mutableMapOf()) { (point) ->
             if (point.y >= y) {
-                Point(point.x, point.y + 1)
+                Point(point.x, point.y + grow)
             } else {
                 point
+            }
+        }.onEach { (p, k) ->
+            k.coord = p
+        }
+        dim = Point(dim.x, dim.y + grow)
+        recalcPoints()
+    }
+
+    private fun recalcPoints() {
+        rowsWithPoints.clear()
+        colsWithPoints.clear()
+        space.forEach { (p, k) ->
+            if (k is Kind.Galaxy) {
+                rowsWithPoints.add(p.y)
+                colsWithPoints.add(p.x)
             }
         }
     }
 
-    fun shiftColumn(x: Int) {
+
+
+    fun shiftColumn(x: Long) {
         space = space.mapKeysTo(mutableMapOf()) { (point) ->
             if (point.x >= x) {
-                Point(point.x + 1, point.y)
+                Point(point.x + grow, point.y)
             } else {
                 point
             }
+        }.onEach { (p, k) ->
+            k.coord = p
         }
+        recalcPoints()
+        dim = Point(dim.x + grow, dim.y)
+    }
+
+    fun expand() {
+        recalcPoints()
+
+        var y = 0L
+        while (y <= dim.y) {
+//            val isAllSpaceY =
+//                (0..dim.x).map { px -> Point(px, y) }.map { p -> space[p] }.all { it is Kind.Space || it == null }
+//            println("$y: $isAllSpaceY")
+            val isAllSpaceY = !rowsWithPoints.contains(y)
+            if (isAllSpaceY) {
+                shiftRow(y)
+//                space += (0..dim.x).map { px ->
+//                    val p = Point(px, y)
+//                    p to Kind.Space(".", p)
+//                }
+                y += grow
+            }
+            y += 1
+        }
+
+        var x = 0L
+        while (x <= dim.x) {
+//            val isAllSpaceX =
+//                (0..dim.y).map { py -> Point(x, py) }.map { p -> space[p] }.all { it is Kind.Space || it == null }
+//            println("$x: $isAllSpaceX")
+            val isAllSpaceX = !colsWithPoints.contains(x)
+
+            if (isAllSpaceX) {
+                shiftColumn(x)
+//                space += (0..dim.y).map { py ->
+//                    val p = Point(x, py)
+//                    p to Kind.Space(".", p)
+//                }
+                x += grow
+            }
+            x += 1
+        }
+
+        renumber()
     }
 
     companion object {
         fun expand(source: Universe): Universe {
             val newUniverse = Universe()
-            val space = mutableMapOf<Point, Kind>() + source.space
-            for (y in newUniverse.dim.y downTo 0) {
-                val isAllSpaceY =
-                    (0..newUniverse.dim.x).map { x -> Point(x, y) }.map { p -> space[p] }.all { it is Kind.Space }
-                println("$y: $isAllSpaceY")
-                if (isAllSpaceY) {
-                    newUniverse.shiftRow(y)
-                    newUniverse.space += (0..newUniverse.dim.x).map { x ->
-                        val p = Point(x, y)
-                        p to Kind.Space(".", p)
-                    }
-                }
-            }
-            newUniverse.dim = Point(space.keys.maxOf { it.x }, space.keys.maxOf { it.y })
-            for (x in newUniverse.dim.x downTo 0) {
-                val isAllSpaceX =
-                    (0..newUniverse.dim.y).map { y -> Point(x, y) }.map { p -> space[p] }.all { it is Kind.Space }
-                println("$x: $isAllSpaceX")
-
-                if (isAllSpaceX) {
-                    newUniverse.shiftColumn(x)
-                    newUniverse.space += (0..newUniverse.dim.y).map { y ->
-                        val p = Point(x, y)
-                        p to Kind.Space(".", p)
-                    }
-                }
-            }
-            newUniverse.dim = Point(space.keys.maxOf { it.x }, space.keys.maxOf { it.y })
-
+            newUniverse.space = (mutableMapOf<Point, Kind>() + source.space).toMutableMap()
+            newUniverse.dim = source.dim
+            newUniverse.expand()
             return newUniverse
         }
     }
+}
 
-    fun main() {
-        val day = "11"
+fun main() {
+    val day = "11"
 
-        fun createUniverse(input: List<String>): Universe {
-            val universe = Universe()
-            var p: Point? = null
-            for ((y, line) in input.withIndex()) {
-                for ((x, char) in line.withIndex()) {
-                    p = Point(x, y)
-                    when (char) {
-                        '.' -> universe.space[p] = Kind.Space(char.toString(), p)
-                        '#' -> universe.space[p] = Kind.Galaxy(char.toString(), p)
-                        else -> error("Unknown char $char")
-                    }
+    fun createUniverse(input: List<String>): Universe {
+        val universe = Universe()
+        var p: Point? = null
+        for ((y, line) in input.withIndex()) {
+            for ((x, char) in line.withIndex()) {
+                p = Point(x.toLong(), y.toLong())
+                when (char) {
+                    '.' -> universe.space[p] = Kind.Space(char.toString(), p)
+                    '#' -> universe.space[p] = Kind.Galaxy(char.toString(), p)
+                    else -> error("Unknown char $char")
                 }
             }
-            universe.dim = p!!
-            return universe
         }
+        universe.dim = p!!
+        return universe
+    }
 
-        fun part1(input: List<String>): Any {
-            val universe = createUniverse(input)
-            val expandedUniverse = Universe.expand(universe)
-            expandedUniverse.printSpace()
-            return input.size
+    fun part1(input: List<String>): Any {
+        val u1 = createUniverse(input)
+        val universe = Universe.expand(u1)
+//        universe.printSpace()
+//        for ((g1, g2) in listOf(
+//            universe.galaxyByNumber[5]!! to universe.galaxyByNumber[9]!!,
+//            universe.galaxyByNumber[1]!! to universe.galaxyByNumber[7]!!,
+//            universe.galaxyByNumber[3]!! to universe.galaxyByNumber[6]!!,
+//            universe.galaxyByNumber[8]!! to universe.galaxyByNumber[9]!!,
+//        )) {
+//            println("${g1.num}, ${g2.num} -> ${g1.coord.distanceTo(g2.coord)}")
+//        }
+        val x = universe.pairs.sumOf { (g1, g2) ->
+            g1.coord.distanceTo(g2.coord)
         }
+        println("Sum: ${x}")
+        return x
+    }
 
-        fun part2(input: List<String>): Any {
-            return input.size
-        }
+    fun part2(input: List<String>): Any {
+        return input.size
+    }
 
-        // test if implementation meets criteria from the description, like:
-        val testInput = readInput("Day${day}_test")
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day${day}_test")
 
-        val part1Expected = ""
-        val part1 = part1(testInput)
-        println("(Test) Part 1: expected: $part1Expected, got: $part1")
+    val part1Expected = "1030"
+    val part1 = part1(testInput)
+    println("(Test) Part 1: expected: $part1Expected, got: $part1")
 
 //    val part2Expected = ""
 //    val part2 = part2(testInput)
 //    println("(Test) Part 2: expected: $part2Expected, got: $part2")
 //
-//    val input = readInput("Day${day}")
+    val input = readInput("Day${day}")
 //
-//    val part1Real = part1(input)
-//    println("(Real) Part 1: $part1Real")
+    val part1Real = part1(input)
+    println("(Real) Part 1: 9684228 $part1Real")
 //
 //    val part2Real = part2(input)
 //    println("(Real) Part 2: $part2Real")
-    }
 }
+
