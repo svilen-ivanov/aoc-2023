57efa1e75667e57d4e1ec6b91a4d0b637c12507a
Svilen Ivanov <svilen.ivanov@gmail.com>
2023-12-16 08:03:05 +0200

    Day 16

--- src/aoc2023/day16/Day16.kt
+++ src/aoc2023/day16/Day16.kt
@@ -2,32 +2,96 @@ package aoc2023.day16
 
 import readInput
 
-data class Point(val x: Int, val y: Int)
+data class Point(val x: Int, val y: Int) {
+    operator fun plus(other: Point): Point {
+        return Point(x + other.x, y + other.y)
+    }
+}
+
+data class Vector(
+    val direction: Compass,
+    val point: Point,
+) {
+    fun next() = Vector(direction, point + direction.next)
+}
+
+enum class Compass(val next: Point) {
+    NORTH(Point(0, -1)),
+    EAST(Point(1, 0)),
+    SOUTH(Point(0, 1)),
+    WEST(Point(-1, 0));
+
+    fun vector() = Vector(this, next)
+}
+
+typealias DirectionLambda = (Compass) -> Compass
+
+val right: DirectionLambda = {
+    when (it) {
+        Compass.WEST -> Compass.SOUTH
+        Compass.SOUTH -> Compass.WEST
+        Compass.EAST -> Compass.NORTH
+        Compass.NORTH -> Compass.EAST
+    }
+}
+
+val left: DirectionLambda = {
+    when (it) {
+        Compass.WEST -> Compass.NORTH
+        Compass.NORTH -> Compass.WEST
+        Compass.EAST -> Compass.SOUTH
+        Compass.SOUTH -> Compass.EAST
+    }
+}
 
 sealed class Item {
     abstract val position: Point
     abstract val symbol: String
 
+    abstract fun changeDirection(compass: Compass): List<Compass>
     data class Empty(override val position: Point) : Item() {
         override val symbol = "."
+        override fun changeDirection(compass: Compass) = listOf(compass)
     }
 
     sealed class Mirror : Item()
     data class LeftMirror(override val position: Point) : Mirror() {
+
+        override fun changeDirection(compass: Compass): List<Compass> {
+            return listOf(left(compass))
+        }
+
         override val symbol = "\\"
     }
 
     data class RightMirror(override val position: Point) : Mirror() {
         override val symbol = "/"
+        override fun changeDirection(compass: Compass): List<Compass> {
+            return listOf(right(compass))
+        }
     }
 
     sealed class Splitter : Item()
     data class VerticalSplit(override val position: Point) : Splitter() {
         override val symbol = "|"
+
+        override fun changeDirection(compass: Compass): List<Compass> {
+            return when (compass) {
+                Compass.NORTH, Compass.SOUTH -> listOf(compass)
+                Compass.EAST, Compass.WEST -> listOf(left(compass), right(compass))
+            }
+        }
+
     }
 
     data class HorizontalSplit(override val position: Point) : Splitter() {
         override val symbol = "-"
+        override fun changeDirection(compass: Compass): List<Compass> {
+            return when (compass) {
+                Compass.NORTH, Compass.SOUTH -> listOf(left(compass), right(compass))
+                Compass.EAST, Compass.WEST -> listOf(compass)
+            }
+        }
     }
 }
 
@@ -48,8 +112,33 @@ data class Contraption(val map: MutableMap<Point, Item>) {
                 appendLine()
             }
         }
-
     }
+}
+
+class Solve(val contraption: Contraption) {
+    val visited = mutableSetOf<Vector>()
+    fun solve(current: Vector) {
+        while (true) {
+            val item = contraption.map[current.point]
+            if (item == null) {
+                println("Out of bounds")
+                return
+            }
+            visited.add(current)
+
+            val newDirections = item.changeDirection(current.direction)
+            newDirections.forEach {
+                val newVector = Vector(it, current.point + it.next)
+                if (visited.contains(newVector)) {
+                    println("Loop")
+                    return
+                }
+                solve(newVector)
+            }
+
+        }
+    }
+
 }
 
 fun main() {
@@ -73,10 +162,11 @@ fun main() {
         }
         return contraption
     }
+
     fun part1(input: List<String>): Any {
         val contraption = parse(input)
         println(contraption)
-        return  contraption
+        return contraption
     }
 
     fun part2(input: List<String>): Any {
