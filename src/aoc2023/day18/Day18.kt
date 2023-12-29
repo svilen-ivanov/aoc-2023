c44d7cb9ee1e168b8cb9e134e2a362ef3439c2c4
Svilen Ivanov <svilen.ivanov@gmail.com>
2023-12-29 15:44:04 +0200

    WIP

--- src/aoc2023/day18/Day18.kt
+++ src/aoc2023/day18/Day18.kt
@@ -1,7 +1,11 @@
 package aoc2023.day18
 
 import readInput
+import size
 import kotlin.math.abs
+import kotlin.math.absoluteValue
+import kotlin.math.max
+import kotlin.math.min
 
 sealed class Direction {
     abstract val symbol: String
@@ -53,7 +57,7 @@ class DigPlan {
     override fun toString(): String {
         return buildString {
             for (command in commands) {
-                appendLine("${command.direction.symbol} ${command.distance} (#${command.color.hex})")
+                appendLine("${command.direction.symbol} ${command.distance}")
             }
         }
     }
@@ -71,7 +75,52 @@ data class Point(val x: Long, val y: Long) {
     operator fun times(other: Long): Point {
         return Point(x * other, y * other)
     }
+
+    fun outline(): List<Point> {
+        return listOf(
+            this,
+            Point(x + 1, y),
+            Point(x + 1, y + 1),
+            Point(x, y + 1),
+        )
+    }
+
+    fun edge(dir: Direction): List<Point> {
+        return when (dir) {
+            Direction.Up -> listOf(this, Point(x + 1, y))
+            Direction.Down -> listOf(Point(x, y + 1), Point(x + 1, y + 1))
+            Direction.Right -> listOf(Point(x + 1, y), Point(x + 1, y + 1))
+            Direction.Left -> listOf(this, Point(x, y + 1))
+        }
+    }
+
+    fun reverseEdge(dir: Direction): List<Point> {
+        return edge(
+            when (dir) {
+                Direction.Up -> Direction.Down
+                Direction.Down -> Direction.Up
+                Direction.Right -> Direction.Left
+                Direction.Left -> Direction.Right
+            }
+        )
+    }
 }
+
+class Trench {
+    val topWall = mutableListOf<Point>()
+    val bottomWall = mutableListOf<Point>()
+
+    override fun toString(): String {
+        return buildString {
+            appendLine("---")
+            appendLine("Trench:")
+            appendLine("  top: $topWall")
+            appendLine("  bottom: $bottomWall")
+            appendLine("---")
+        }
+    }
+}
+
 
 data class Line(val p1: Point, val p2: Point) {
     init {
@@ -92,37 +141,34 @@ data class Line(val p1: Point, val p2: Point) {
     }
 }
 
-fun extra(oldDir: Direction, newDir: Direction): Point? {
-    return if (oldDir == Direction.Right) {
-        when (newDir) {
-            is Direction.Up -> return Point(0, 0)
-            is Direction.Down -> return Point(1, 0)
-            else -> return Point(0, 0)
-        }
-    } else {
-        null
-    }
-}
-
-
 class ExecuteDigPlan(val digPlan: DigPlan) {
     var edges = mutableListOf<Point>()
     var lines = mutableListOf<Line>()
-//    lateinit var verticalLines: List<Line>
+    val trenches = mutableListOf<Trench>()
     var topLeft: Point = Point(0, 0)
     var bottomRight: Point = Point(0, 0)
     fun execute() {
-        for ((i, command) in digPlan.commands.withIndex()) {
-            if (i == 0) {
-                edges += Point(0, 0)
-            }
+        edges += Point(0, 0)
+        for (command in digPlan.commands) {
             val prev = edges.last()
             val next = prev + command.direction.vector(command.distance)
+
+            val trench = Trench()
+            val (top1, bottom1) = prev.edge(command.direction)
+            trench.topWall += top1
+            trench.bottomWall += bottom1
+
+            val (top2, bottom2) = next.reverseEdge(command.direction)
+            trench.topWall += top2
+            trench.bottomWall += bottom2
+
+            trenches += trench
+
             val line = Line(prev, next)
             lines += line
             edges += next
-            bottomRight = Point(maxOf(bottomRight.x, next.x), maxOf(bottomRight.y, next.y))
             topLeft = Point(minOf(topLeft.x, next.x), minOf(topLeft.y, next.y))
+            bottomRight = Point(maxOf(bottomRight.x, next.x), maxOf(bottomRight.y, next.y))
         }
         require(edges.last() == edges.first()) {
             "Path must end where it started: ${edges.last()} != ${edges.first()}"
@@ -146,6 +192,139 @@ class ExecuteDigPlan(val digPlan: DigPlan) {
         }
     }
 
+    fun connectTrenches(): Long {
+        val outline1 = mutableListOf<Point>()
+        val outline2 = mutableListOf<Point>()
+        for (i in 0 until trenches.size) {
+            val current = trenches[i]
+
+            if (outline1.isEmpty()) {
+                outline1 += current.topWall
+                outline2 += current.bottomWall
+                continue
+            }
+
+            val r1 = appendOutline(outline1, current)
+            val r2 = appendOutline(outline2, current)
+            require(r1.isNotEmpty() || r2.isNotEmpty()) {
+                "Trenches must be connected: $current"
+            }
+
+            if (r1.isNotEmpty()) {
+                val last = outline2.last()
+                val (common, start, end) = r1
+                outline2 += join(common, last, start) + listOf(end)
+            } else {
+                val last = outline1.last()
+                val (common, start, end) = r2
+                outline1 += join(common, last, start) + listOf(end)
+            }
+        }
+
+        println("Outline1: $outline1: ${areaOf(outline1)}}")
+//        printOutline(outline1)
+        println("Outline2: $outline2: ${areaOf(outline2)}}")
+
+        return max(areaOf(outline1), areaOf(outline2))
+//        printOutline(outline2)
+    }
+
+    fun areaOf(points: List<Point>): Long {
+        return points.windowed(2).sumOf { (a, b) ->
+            (a.y + b.y) * (a.x - b.x)
+        } / 2
+    }
+
+    fun printOutline(outline: List<Point>) {
+        for (y in topLeft.y - 10..bottomRight.y + 10) {
+            for (x in topLeft.x - 10..bottomRight.x + 10) {
+                val point = Point(x, y)
+                val onLine = outline.contains(point)
+                if (!onLine) {
+                    print(".")
+                } else {
+                    print("#")
+                }
+            }
+            println()
+        }
+    }
+
+
+    private fun appendOutline(outline: MutableList<Point>, current: Trench): List<Point> {
+        val last = outline.last()
+        if (last == current.topWall[0]) {
+            outline += current.topWall[1]
+            return listOf(last, current.bottomWall[0], current.bottomWall[1])
+        } else if (last == current.bottomWall[0]) {
+            outline += current.bottomWall[1]
+            return listOf(last, current.topWall[0], current.topWall[1])
+        } else {
+            return emptyList()
+        }
+    }
+
+    private fun findMissing(common: Point, last: Point, start: Point): Point {
+        require((last.x - start.x).absoluteValue == 1L && (last.y - start.y).absoluteValue == 1L)
+
+        val p1 = Point(min(start.x, last.x), min(start.y, last.y))
+        val p2 = Point(max(start.x, last.x), max(start.y, last.y))
+        val p3 = Point(min(start.x, last.x), max(start.y, last.y))
+        val p4 = Point(max(start.x, last.x), min(start.y, last.y))
+
+        return listOf(p1, p2, p3, p4).first { it != common && it != start && it != last }
+    }
+
+    private fun join(common: Point, last: Point, start: Point): List<Point> {
+        return listOf(
+            findMissing(common, last, start),
+            start
+        )
+    }
+
+
+    fun fill(): Long {
+        var count = 0L
+        val verticalLines = lines.filter { it.p1.x == it.p2.x }
+        val horizontalLines = lines.filter { it.p1.y == it.p2.y }
+        for (y in topLeft.y..bottomRight.y) {
+            var crossCount = 0L
+            var x = topLeft.x
+            while (x <= bottomRight.x) {
+                val point = Point(x, y)
+                val horizLine = horizontalLines.firstOrNull { it.liesOnLine(point) }
+                if (horizLine != null) {
+                    count += (horizLine.p1.x - horizLine.p2.x).absoluteValue.toInt() + 1
+                    val vert1 =
+                        verticalLines.first { (it.p1.y == horizLine.p1.y || it.p2.y == horizLine.p1.y) && (it.p1.x == horizLine.p1.x || it.p2.x == horizLine.p1.x) }
+                    val vert2 =
+                        verticalLines.first { (it.p1.y == horizLine.p1.y || it.p2.y == horizLine.p1.y) && (it.p1.x == horizLine.p2.x || it.p2.x == horizLine.p2.x) }
+                    val minY = minOf(vert1.p1.y, vert1.p2.y, vert2.p1.y, vert2.p2.y)
+                    val maxY = maxOf(vert1.p1.y, vert1.p2.y, vert2.p1.y, vert2.p2.y)
+                    if (minY < y && y < maxY) {
+                        crossCount++
+                    }
+                    x = maxOf(horizLine.p1.x, horizLine.p2.x) + 1
+                } else {
+                    val onVertLine = verticalLines.any { it.liesOnLine(point) }
+                    if (onVertLine) {
+                        if (crossCount % 2L == 1L) {
+                            count++
+                        }
+                        crossCount++
+                    }
+
+                    if (crossCount % 2L == 1L) {
+                        count++
+                    }
+                    x++
+                }
+
+            }
+        }
+        return count
+    }
+
     fun length(): Long {
         return edges.windowed(2).sumOf { (a, b) ->
             val r = (b - a)
@@ -159,9 +338,9 @@ class ExecuteDigPlan(val digPlan: DigPlan) {
         }
     }
 
-    fun area(): Double {
+    fun area(): Long {
         return edges.windowed(2).sumOf { (a, b) ->
-            (a.y + b.y) * (a.x - b.x).toDouble()
+            (a.y + b.y) * (a.x - b.x)
         } / 2
     }
 }
@@ -192,12 +371,35 @@ fun main() {
         return digPlan
     }
 
+    fun convert(digPlanOld: DigPlan): DigPlan {
+        val digPlan = DigPlan()
+        digPlanOld.commands.forEach {
+            val distance = it.color.hex.substring(0..4).toLong(16)
+            val code = it.color.hex.last()
+            val dir = when (code) {
+                '0' -> Direction.Right
+                '1' -> Direction.Down
+                '2' -> Direction.Left
+                '3' -> Direction.Up
+                else -> error("Unknown direction: $code")
+            }
+            println(distance)
+            digPlan.addCommand(Command(dir, distance, it.color))
+        }
+        println(digPlan)
+        return digPlan
+    }
+
     fun part1(input: List<String>): Any? {
-        val digPlan = parse(input)
+        val digPlanOld = parse(input)
+        val digPlan = convert(digPlanOld)
         val digPlanExecute = ExecuteDigPlan(digPlan)
+//        val digPlanExecute = ExecuteDigPlan(digPlanOld)
         digPlanExecute.execute()
-        digPlanExecute.printPlan()
-        return 0
+//        digPlanExecute.printPlan()
+        val res = digPlanExecute.connectTrenches()
+//        val res = digPlanExecute.area()
+        return res
     }
 
     fun part2(input: List<String>): Any? {
@@ -215,10 +417,10 @@ fun main() {
 //    val part2 = part2(testInput)
 //    println("(Test) Part 2: expected: $part2Expected, got: $part2")
 
-//    val input = readInput(day, "Day${day}")
+    val input = readInput(day, "Day${day}")
 
-//    val part1Real = part1(input)
-//    println("(Real) Part 1: $part1Real")
+    val part1Real = part1(input)
+    println("(Real) Part 1: $part1Real")
 
 //    val part2Real = measureTimedValue { part2(input) }
 //    println("(Real) Part 2: $part2Real")
